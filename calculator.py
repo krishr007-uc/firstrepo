31aa763558b152192e0b89a0649da415fd683e44
krishr007-uc <glacierom@gmail.com>
2026-06-19 18:40:45 +0000

    fixed goodbye spelling

--- calculator.py
+++ calculator.py
@@ -1,6 +1,7 @@
-x=input("What's x?")
-y=input("What's y?")
-
-z=round(float(x)/float(y),3)
-
-print(f"{z:.2f}")
+x = input("What's x?")
+y = input("What's y?")
+if float(y) == 0:
+    print("Error: cannot divide by zero")
+else:
+    z = round(float(x) / float(y), 3)
+    print(f"{z:.2f}")
